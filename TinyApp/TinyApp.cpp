450f67e686ee74e0e8527ebd508953279f305c98
shuangshu <njshuangshu@sina.com>
2016-01-29 22:30:18 +0800

    update ui

--- TinyApp/TinyApp.cpp
+++ TinyApp/TinyApp.cpp
@@ -13,126 +13,14 @@
 #pragma comment(lib,"TinyUI.lib")
 using namespace TinyUI;
 
-//class ListV
-//{
-//public:
-//	ListV()
-//		:V(0)
-//	{
-//		TRACE("ListV默认构造函数\n");
-//	}
-//	~ListV()
-//	{
-//		TRACE("ListV析构函数:%d\n",V);
-//	}
-//	explicit ListV(INT v)
-//		:V(v)
-//	{
-//		TRACE("ListV构造函数:%d\n",V);
-//	}
-//	ListV(const ListV& other)
-//		:V(other.V)
-//	{
-//		TRACE("ListV拷贝构造函数:%d\n",V);
-//	}
-//	ListV& operator=(const ListV& other)
-//	{
-//		TRACE("ListV赋值构造函数\n");
-//		return *this;
-//	}
-//	ListV(ListV&& other)
-//		:V(other.V)
-//	{
-//		TRACE("ListV移动构造函数\n");
-//	}
-//
-//	static ListV GetV(INT v)
-//	{
-//		return ListV(v);
-//	}
-//
-//	static ListV GetV1(INT v)
-//	{
-//		ListV v1(v);
-//		return v1;
-//	}
-//
-//public:
-//	INT V;
-//};
-//template<>
-//class DefaultTraits < ListV >
-//{
-//public:
-//	static INT  Compare(const ListV& element1, const ListV& element2)
-//	{
-//		if (element1.V == element2.V)
-//		{
-//			return 0;
-//		}
-//		else if (element1.V > element2.V)
-//		{
-//			return 1;
-//		}
-//		return -1;
-//	}
-//};
-/*TinyTreeMap<INT, ListV> maps;
-for (int i = 0; i < 100; i++)
+class Test
 {
-ListV lv(i);
-if (i == 99)
-{
-maps.Add(i, lv);
-}
-else
-{
-maps.Add(i, lv);
-}
-}
-ITERATOR a = maps.First();
-while (a)
-{
-INT k = maps.GetKeyAt(a);
-TRACE("K:%d,V:%d\n",k , maps.GetValueAt(a).V);
-a = maps.Next(a);
-}
-INT v = 45;
-a = maps.Lookup(v);*/
-/*TinyLinkList<ListV> linkList;
-for (int i = 0; i < 100; i++)
-{
-ListV lv(i);
-if (i == 99)
-{
-linkList.InsertLast(lv);
-}
-else
-{
-linkList.InsertLast(lv);
-}
-}
-ITERATOR a = linkList.First();
-while (a)
-{
-TRACE("V:%d\n", linkList.GetAt(a).V);
-a = linkList.Next(a);
-}
-ListV v(45);
-a = linkList.Lookup(v);
-TRACE("V-Lookup:%d\n", linkList.GetAt(a).V);
-linkList.RemoveAt(a);
-a = linkList.First();
-while (a)
-{
-TRACE("V:%d\n", linkList.GetAt(a).V);
-a = linkList.Next(a);
-}
-linkList.RemoveAll();*/
-/*ListV v(49);
-ITERATOR os = linkList.Lookup(v, NULL);
-ListV& lv = linkList.GetAt(os);*/
-
+public:
+	void traceMsg(TinyString str)
+	{
+		TRACE(str.STR());
+	}
+};
 
 INT APIENTRY _tWinMain(HINSTANCE hInstance,
 	_In_opt_ HINSTANCE hPrevInstance,
@@ -142,25 +30,12 @@ INT APIENTRY _tWinMain(HINSTANCE hInstance,
 	UNREFERENCED_PARAMETER(hPrevInstance);
 	UNREFERENCED_PARAMETER(lpCmdLine);
 
+	Test test;
+	Callback<void(TinyString)> abc = BindCallback(&Test::traceMsg, &test);
+	abc("abc");
+
 	WSADATA   wsd;
 	WSAStartup(MAKEWORD(2, 2), &wsd);
-
-	/*FolderWatcher watcher;
-	watcher.Initialize();
-	watcher.Watch("D:\\pdf1");
-	watcher.Watch("D:\\pdf2");*/
-	//Windowless::TinyVisual visual;
-	//visual.ParserAttributes();
-	//TinyString str = visual.ToStyle();
-	////TinyString* str = visual.GetAttribute("font-size");
-	////TinyString m_style("font-family:arial;color:red;font-size:20px;");
-	////TinyArray<TinyString> _Array;
-	////m_style.Split(';', _Array);
-	////TinyMap<TinyString, TinyString> m_attrMap;//属性映射
-	////for (INT i = 0; i < _Array.GetSize(); i++)
-	////{
-	////	m_attrMap.Add(_Array[i], m_style);
-	////}
 
 	HRESULT hRes = OleInitialize(NULL);
 	::DefWindowProc(NULL, 0, 0, 0L);
--- TinySmiley/smiley_h.h
+++ TinySmiley/smiley_h.h
@@ -4,7 +4,7 @@
 
 
  /* File created by MIDL compiler version 8.00.0603 */
-/* at Sat Nov 14 18:34:51 2015
+/* at Wed Jan 27 21:33:48 2016
  */
 /* Compiler settings for smiley.idl:
     Oicf, W1, Zp8, env=Win32 (32b run), target_arch=X86 8.00.0603 
