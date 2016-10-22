1104fc9a7d8c0112e8d3bd10b214c91bfc0c126f
shuangshu <njshuangshu@sina.com>
2016-10-22 12:04:03 +0800

    update

---
+++ DXCapture/DX101Capture.cpp
@@ -0,0 +1,13 @@
+#include "stdafx.h"
+#include "DX101Capture.h"
+
+namespace DXCapture
+{
+	DX101Capture::DX101Capture()
+	{
+	}
+
+	DX101Capture::~DX101Capture()
+	{
+	}
+}
---
+++ DXCapture/DX101Capture.h
@@ -0,0 +1,12 @@
+#pragma once
+
+namespace DXCapture
+{
+	class DX101Capture
+	{
+	public:
+		DX101Capture();
+		~DX101Capture();
+	};
+}
+
