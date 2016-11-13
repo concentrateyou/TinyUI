5e1f8eb2c140c5dd0b108db824baea73895ced9c
csy <csy@CSY-PC>
2016-11-13 18:26:58 +0800

    update

---
+++ TinyUI/Media/TinyAudioMixer.cpp
@@ -0,0 +1,15 @@
+#include "../stdafx.h"
+#include "TinyMFH264Encode.h"
+
+namespace TinyUI
+{
+	namespace Media
+	{
+
+		void TinyAudioMixer::OnDataAvailable(BYTE* bits, LONG size, LPVOID lpParameter)
+		{
+			throw std::logic_error("The method or operation is not implemented.");
+		}
+
+	};
+}
