07b3b638529dc73ae57fd7fd2b163d9f5b803154
shuangshu <njshuangshu@sina.com>
2016-12-08 15:32:53 +0800

    update

---
+++ OpenGL/OpenGL.h
@@ -0,0 +1,9 @@
+#pragma once
+#include "resource.h"
+#include <gl/GL.h>
+#include <gl/GLU.h>
+#include <gl/glut.h>
+#pragma comment(lib, "opengl32.lib")
+#pragma comment(lib, "glu32.lib")
+#pragma comment(lib, "glu32.lib")
+
