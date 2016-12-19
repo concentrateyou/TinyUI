f4dceab30bacda27af60a62beb4e0f5f9cd70cbb
shuangshu <njshuangshu@sina.com>
2016-12-19 10:10:37 +0800

    update

--- AudioToolkit/AudioToolkit.cpp
+++ AudioToolkit/AudioToolkit.cpp
@@ -3,4 +3,95 @@
 
 #include "stdafx.h"
 #include "AudioToolkit.h"
+#include "Windowless/TinyVisualHWND.h"
+#include "Windowless/TinyVisualRichText.h"
+#include "Render/TinyDDraw.h"
+#include "Media/TinyWave.h"
+#include "Media/TinyWASAPIAudioCapture.h"
+#include "Media/TinyWASAPIAudioRender.h"
+#include "Media/TinyAudioDSPCapture.h"
+#include "Network/TinyIOServer.h"
+#include "Network/TinySocket.h"
+#include "Network/TinyHTTPClient.h"
+using namespace TinyUI;
 
+BOOL LoadSeDebugPrivilege()
+{
+	DWORD   err;
+	HANDLE  hToken;
+	LUID    Val;
+	TOKEN_PRIVILEGES tp;
+	if (!OpenProcessToken(GetCurrentProcess(), TOKEN_ADJUST_PRIVILEGES | TOKEN_QUERY, &hToken))
+	{
+		err = GetLastError();
+		return FALSE;
+	}
+	if (!LookupPrivilegeValue(NULL, SE_DEBUG_NAME, &Val))
+	{
+		err = GetLastError();
+		CloseHandle(hToken);
+		return FALSE;
+	}
+
+	tp.PrivilegeCount = 1;
+	tp.Privileges[0].Luid = Val;
+	tp.Privileges[0].Attributes = SE_PRIVILEGE_ENABLED;
+	if (!AdjustTokenPrivileges(hToken, FALSE, &tp, sizeof(tp), NULL, NULL))
+	{
+		err = GetLastError();
+		CloseHandle(hToken);
+		return FALSE;
+	}
+	CloseHandle(hToken);
+	return TRUE;
+}
+
+TinyUI::Media::TinyWaveFile waveFile;
+WAVEFORMATEX* waveFormat;
+
+void CaptureCB(BYTE* bits, LONG size, LPVOID ps)
+{
+	waveFile.Write(bits, size);
+}
+
+void ReadCB(BYTE* bits, LONG size, LPVOID ps)
+{
+	LONG numberOfBytesRead = 0;
+	waveFile.Read(bits, size, &numberOfBytesRead);
+}
+
+
+INT APIENTRY _tWinMain(HINSTANCE hInstance,
+	_In_opt_ HINSTANCE hPrevInstance,
+	LPTSTR    lpCmdLine,
+	INT       nCmdShow)
+{
+
+	UNREFERENCED_PARAMETER(hPrevInstance);
+	UNREFERENCED_PARAMETER(lpCmdLine);
+
+	WSADATA   wsd;
+	WSAStartup(MAKEWORD(2, 2), &wsd);
+	MFStartup(MF_VERSION);
+
+	HRESULT hRes = OleInitialize(NULL);
+
+	LoadSeDebugPrivilege();
+
+	::DefWindowProc(NULL, 0, 0, 0L);
+	TinyApplication::GetInstance()->Initialize(hInstance, lpCmdLine, nCmdShow, MAKEINTRESOURCE(IDC_AUDIOTOOLKIT));
+	TinyMessageLoop theLoop;
+	TinyApplication::GetInstance()->AddMessageLoop(&theLoop);
+	Windowless::TinyVisualHWND uiImpl;
+	uiImpl.Create(NULL, 50, 50, 800, 600);
+	uiImpl.ShowWindow(nCmdShow);
+	uiImpl.UpdateWindow();
+	INT loopRes = theLoop.MessageLoop();
+	TinyApplication::GetInstance()->RemoveMessageLoop();
+	TinyApplication::GetInstance()->Uninitialize();
+
+	OleUninitialize();
+	MFShutdown();
+	WSACleanup();
+	return loopRes;
+};
