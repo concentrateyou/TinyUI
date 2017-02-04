89f1a223fa4411e558d64eed9d5ba83873ba2421
shuangshu <njshuangshu@sina.com>
2017-02-04 15:45:45 +0800

    update

---
+++ Encode/FLVEncode.cpp
@@ -0,0 +1,13 @@
+#include "stdafx.h"
+#include "FLVEncode.h"
+
+namespace Encode
+{
+	FLVEncode::FLVEncode()
+	{
+	}
+
+	FLVEncode::~FLVEncode()
+	{
+	}
+}
---
+++ Encode/FLVEncode.h
@@ -0,0 +1,15 @@
+#pragma once
+
+namespace Encode
+{
+
+
+	class FLVEncode
+	{
+	public:
+		FLVEncode();
+		~FLVEncode();
+	};
+}
+
+
