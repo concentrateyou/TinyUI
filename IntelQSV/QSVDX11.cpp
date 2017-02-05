16c951d9add621fc42b8dcebe637fd28a8d09b64
csy <csy@CSY-PC>
2017-02-05 17:13:42 +0800

    update

---
+++ IntelQSV/QSVDX11.cpp
@@ -0,0 +1,13 @@
+#include "stdafx.h"
+#include "QSVDX11.h"
+
+namespace QSV
+{
+	QSVDX11::QSVDX11()
+	{
+	}
+
+	QSVDX11::~QSVDX11()
+	{
+	}
+}
---
+++ IntelQSV/QSVDX11.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "Common/TinyCommon.h"
+#include <windows.h>
+#include <d3d11_1.h>
+#include <dxgi1_2.h>
+#include "mfxvideo++.h"
+using namespace TinyUI;
+
+namespace QSV
+{
+	class QSVDX11
+	{
+	public:
+		QSVDX11();
+		~QSVDX11();
+	};
+}
+
+
+
