b8812cba2eb6c6377fcd874a98ba8e6130aa058f
shuangshu <njshuangshu@sina.com>
2017-03-16 11:55:07 +0800

    update

---
+++ TinyUI/IO/TinyPipe.cpp
@@ -0,0 +1,12 @@
+#include "../stdafx.h"
+#include <process.h>
+#include "TinyPipe.h"
+#include <atomic>
+
+namespace TinyUI
+{
+	namespace IO
+	{
+		
+	}
+}
---
+++ TinyUI/IO/TinyPipe.h
@@ -0,0 +1,25 @@
+#pragma once
+#include "../Common/TinyCollection.h"
+#include "../Common/TinyCore.h"
+#include "../Common/TinyCallback.h"
+#include "../Common/TinyEvent.h"
+#include <string>
+#include <algorithm>
+using namespace std;
+
+namespace TinyUI
+{
+	namespace IO
+	{
+		/// <summary>
+		/// ¹ÜµÀ
+		/// </summary>
+		class TinyPipe
+		{
+			DISALLOW_COPY_AND_ASSIGN(TinyPipe)
+		
+		};
+
+	};
+}
+
