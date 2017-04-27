76619aaed49d75df94580ac03714186ebbfcbfe3
csy <csy@CSY-PC>
2017-04-27 21:58:13 +0800

    update

--- DXFramework/DX11RenderTexture2D.cpp
+++ DXFramework/DX11RenderTexture2D.cpp
@@ -10,51 +10,54 @@ namespace DXFramework
 	DX11RenderTexture2D::~DX11RenderTexture2D()
 	{
 	}
-	BOOL DX11RenderTexture2D::Create(DX11& dx11, INT cx, INT cy)
+	BOOL DX11RenderTexture2D::Create(DX11& dx11)
 	{
-		m_texture2D.Release();
-		m_renderView.Release();
-		m_resourceView.Release();
+		TinyComPtr<ID3D11Texture2D> texture2D;
+		HRESULT hRes = dx11.GetSwap()->GetBuffer(0, __uuidof(ID3D11Texture2D), (LPVOID*)&texture2D);
+		if (hRes != S_OK)
+			return FALSE;
+		hRes = dx11.GetD3D()->CreateRenderTargetView(texture2D, NULL, &m_renderView);
+		if (hRes != S_OK)
+			return FALSE;
 		D3D11_TEXTURE2D_DESC desc;
-		ZeroMemory(&desc, sizeof(desc));
-		desc.Width = cx;
-		desc.Height = cy;
-		desc.MipLevels = 1;
-		desc.ArraySize = 1;
-		desc.Format = DXGI_FORMAT_R32G32B32A32_FLOAT;
-		desc.SampleDesc.Count = 1;
-		desc.Usage = D3D11_USAGE_DEFAULT;
-		desc.BindFlags = D3D11_BIND_RENDER_TARGET | D3D11_BIND_SHADER_RESOURCE;
-		desc.CPUAccessFlags = 0;
-		desc.MiscFlags = 0;
-		HRESULT hRes = dx11.GetD3D()->CreateTexture2D(&desc, NULL, &m_texture2D);
+		texture2D->GetDesc(&desc);
+		D3D11_TEXTURE2D_DESC depthDesc;
+		ZeroMemory(&depthDesc, sizeof(depthDesc));
+		depthDesc.Width = desc.Width;
+		depthDesc.Height = desc.Height;
+		depthDesc.MipLevels = 1;
+		depthDesc.ArraySize = 1;
+		depthDesc.Format = DXGI_FORMAT_D24_UNORM_S8_UINT;
+		depthDesc.SampleDesc.Count = 1;
+		depthDesc.SampleDesc.Quality = 0;
+		depthDesc.Usage = D3D11_USAGE_DEFAULT;
+		depthDesc.BindFlags = D3D11_BIND_DEPTH_STENCIL;
+		depthDesc.CPUAccessFlags = 0;
+		depthDesc.MiscFlags = 0;
+		hRes = dx11.GetD3D()->CreateTexture2D(&depthDesc, NULL, &m_depthTexture2D);
 		if (hRes != S_OK)
 			return FALSE;
-		D3D11_RENDER_TARGET_VIEW_DESC rtvd;
-		ZeroMemory(&rtvd, sizeof(rtvd));
-		rtvd.Format = desc.Format;
-		rtvd.ViewDimension = D3D11_RTV_DIMENSION_TEXTURE2D;
-		rtvd.Texture2D.MipSlice = 0;
-		hRes = dx11.GetD3D()->CreateRenderTargetView(m_texture2D, &rtvd, &m_renderView);
-		if (hRes != S_OK)
-			return FALSE;
-		D3D11_SHADER_RESOURCE_VIEW_DESC dsrvd;
-		ZeroMemory(&dsrvd, sizeof(dsrvd));
-		dsrvd.Format = desc.Format;
-		dsrvd.ViewDimension = D3D11_SRV_DIMENSION_TEXTURE2D;
-		dsrvd.Texture2D.MipLevels = 1;
-		hRes = dx11.GetD3D()->CreateShaderResourceView(m_texture2D, &dsrvd, &m_resourceView);
+		D3D11_DEPTH_STENCIL_VIEW_DESC depthViewDesc;
+		ZeroMemory(&depthViewDesc, sizeof(depthViewDesc));
+		depthViewDesc.Format = DXGI_FORMAT_D24_UNORM_S8_UINT;
+		depthViewDesc.ViewDimension = D3D11_DSV_DIMENSION_TEXTURE2D;
+		depthViewDesc.Texture2D.MipSlice = 0;
+		hRes = dx11.GetD3D()->CreateDepthStencilView(m_depthTexture2D, &depthViewDesc, &m_depthView);
 		if (hRes != S_OK)
 			return FALSE;
 		return TRUE;
 	}
-	void DX11RenderTexture2D::Destory()
+	BOOL DX11RenderTexture2D::Create(DX11& dx11, INT cx, INT cy)
 	{
-		m_renderView.Release();
-		DX11Texture2D::Destory();
+
+		return TRUE;
 	}
-	ID3D11RenderTargetView* DX11RenderTexture2D::GetTRView() const
+	ID3D11RenderTargetView* DX11RenderTexture2D::GetRTView() const
 	{
 		return m_renderView;
+	}
+	ID3D11DepthStencilView* DX11RenderTexture2D::GetDSView() const
+	{
+		return m_depthView;
 	}
 }
--- DXFramework/DX11RenderTexture2D.h
+++ DXFramework/DX11RenderTexture2D.h
@@ -3,17 +3,19 @@
 
 namespace DXFramework
 {
-	class DX11RenderTexture2D : public DX11Texture2D
+	class DX11RenderTexture2D
 	{
 	public:
 		DX11RenderTexture2D();
 		virtual ~DX11RenderTexture2D();
-		virtual BOOL Create(DX11& dx11, INT cx, INT cy);
-		ID3D11RenderTargetView* GetTRView() const;
-	public:
-		void Destory() OVERRIDE;
+		BOOL Create(DX11& dx11);
+		BOOL Create(DX11& dx11, INT cx, INT cy);
+		ID3D11RenderTargetView* GetRTView() const;
+		ID3D11DepthStencilView* GetDSView() const;
 	protected:
+		TinyComPtr<ID3D11Texture2D>			m_depthTexture2D;
 		TinyComPtr<ID3D11RenderTargetView>	m_renderView;
+		TinyComPtr<ID3D11DepthStencilView>	m_depthView;
 	};
 }
 
