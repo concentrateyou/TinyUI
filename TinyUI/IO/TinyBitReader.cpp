630d4d7e5114849cace75c8fda2a20f2c0565521
shuangshu <njshuangshu@sina.com>
2018-01-12 21:16:37 +0800

    update

---
+++ TinyUI/IO/TinyBitReader.cpp
@@ -0,0 +1,80 @@
+﻿#include "../stdafx.h"
+#include "TinyBitReader.h"
+#include <algorithm>
+
+namespace TinyUI
+{
+	namespace IO
+	{
+		TinyBitReader::TinyBitReader(const BYTE* bits, LONG size)
+			:m_bits(bits),
+			m_size(size),
+			m_remainingBits(0)
+		{
+			if (m_size != 0)
+			{
+				m_currentByte = *m_bits;
+				++m_bits;
+				--m_size;
+				m_remainingBits = 8;
+			}
+		}
+
+		TinyBitReader::~TinyBitReader()
+		{
+
+		}
+
+		BOOL TinyBitReader::ReadBitsInternal(INT count, UINT64* out)
+		{
+			*out = 0;
+			while (m_remainingBits != 0 && count != 0)
+			{
+				INT takeBit = min(m_remainingBits, count);
+				*out <<= takeBit;
+				*out += m_currentByte >> (m_remainingBits - takeBit);
+				count -= takeBit;
+				m_remainingBits -= takeBit;
+				m_currentByte &= (1 << m_remainingBits) - 1;
+				if (m_remainingBits == 0)
+				{
+					if (m_size != 0)
+					{
+						m_currentByte = *m_bits;
+						++m_bits;
+						--m_size;
+						m_remainingBits = 8;
+					}
+				}
+			}
+			return (count == 0);
+		}
+
+		BOOL TinyBitReader::SkipBits(INT count)
+		{
+			while (count > 0 && count > m_remainingBits)
+			{
+				count -= m_remainingBits;
+				m_remainingBits = 0;
+				if (m_size != 0)
+				{
+					m_currentByte = *m_bits;
+					++m_bits;
+					--m_size;
+					m_remainingBits = 8;
+				}
+				if (m_remainingBits == 0)
+				{
+					return (count == 0);
+				}
+			}
+			UINT64 value;
+			return ReadBitsInternal(count, &value);
+		}
+
+		UINT32 TinyBitReader::Available() const
+		{
+			return static_cast<UINT32>(8 * m_size + m_remainingBits);
+		}
+	}
+}
---
+++ TinyUI/IO/TinyBitReader.h
@@ -0,0 +1,39 @@
+#pragma once
+#include "../Common/TinyCollection.h"
+#include "../Common/TinyCore.h"
+#include "../Common/TinyCallback.h"
+#include "../Common/TinyEvent.h"
+#include <string>
+#include <algorithm>
+using namespace std;
+
+namespace TinyUI
+{
+	namespace IO
+	{
+		class TinyBitReader
+		{
+		public:
+			TinyBitReader(const BYTE* bits, LONG size);
+			~TinyBitReader();
+			template<typename T>
+			BOOL ReadBits(INT count, T* out)
+			{
+				UINT64 val;
+				BOOL bRes = ReadBitsInternal(count, &val);
+				*out = static_cast<T>(val);
+				return bRes;
+			}
+			BOOL SkipBits(INT count);
+			UINT32 Available() const;
+		private:
+			BOOL ReadBitsInternal(INT count, UINT64* out);
+		private:
+			const BYTE*		m_bits;
+			LONG			m_size;
+			BYTE			m_currentByte;
+			INT				m_remainingBits;
+		};
+	};
+}
+
