21ac29af15d01bf4a0ff606b887f219b4b3d37b2
csy <csy@CSY-PC>
2018-05-31 22:30:48 +0800

    update

---
+++ MFCapture/MFDesktopCapture.cpp
@@ -0,0 +1,16 @@
+#include "stdafx.h"
+#include "MFDesktopCapture.h"
+
+
+namespace MF
+{
+	MFDesktopCapture::MFDesktopCapture()
+	{
+	}
+
+
+	MFDesktopCapture::~MFDesktopCapture()
+	{
+	}
+
+}
---
+++ MFCapture/MFDesktopCapture.h
@@ -0,0 +1,19 @@
+#pragma once
+#include "MFCommon.h"
+
+namespace MF
+{
+	/// <summary>
+	/// ×ÀÃæ²¶»ñ
+	/// </summary>
+	class MFDesktopCapture
+	{
+		DISALLOW_COPY_AND_ASSIGN(MFDesktopCapture)
+	public:
+		MFDesktopCapture();
+		~MFDesktopCapture();
+	};
+}
+
+
+
