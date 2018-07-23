b3281c5bd43dfb2a6f5021fa308eb94980603c17
caoshenyu <caoshenyu@bytedance.com>
2018-07-23 19:42:51 +0800

    update

--- MFCapture/MFCommon.h
+++ MFCapture/MFCommon.h
@@ -4,6 +4,7 @@
 #include "Common/TinyCallback.h"
 #include "Common/TinyCore.h"
 #include "Render/TinyGDI.h"
+#include "Media/TinyMedia.h"
 #include <mmsystem.h>
 #include <dshow.h>
 #include <MMDeviceAPI.h>
@@ -36,44 +37,10 @@
 #pragma comment(lib, "wmcodecdspuuid.lib")
 
 using namespace TinyFramework;
+using namespace TinyFramework::Media;
 
 namespace MF
 {
-	enum VideoPixelFormat
-	{
-		PIXEL_FORMAT_UNKNOWN = 0,
-		PIXEL_FORMAT_I420 = 1,
-		PIXEL_FORMAT_YV12 = 2,
-		PIXEL_FORMAT_YV16 = 3,
-		PIXEL_FORMAT_YV12A = 4,
-		PIXEL_FORMAT_YV24 = 5,
-		PIXEL_FORMAT_NV12 = 6,
-		PIXEL_FORMAT_NV21 = 7,
-		PIXEL_FORMAT_UYVY = 8,
-		PIXEL_FORMAT_YUY2 = 9,
-		PIXEL_FORMAT_ARGB = 10,
-		PIXEL_FORMAT_XRGB = 11,
-		PIXEL_FORMAT_RGB24 = 12,
-		PIXEL_FORMAT_RGB32 = 13,
-		PIXEL_FORMAT_MJPEG = 14,
-		PIXEL_FORMAT_MT21 = 15,
-		PIXEL_FORMAT_YUV420P9 = 16,
-		PIXEL_FORMAT_YUV420P10 = 17,
-		PIXEL_FORMAT_YUV422P9 = 18,
-		PIXEL_FORMAT_YUV422P10 = 19,
-		PIXEL_FORMAT_YUV444P9 = 20,
-		PIXEL_FORMAT_YUV444P10 = 21,
-		PIXEL_FORMAT_MAX = PIXEL_FORMAT_YUV444P10,
-	};
-
-	enum ColorSpace
-	{
-		COLOR_SPACE_UNSPECIFIED = 0,
-		COLOR_SPACE_JPEG = 1,
-		COLOR_SPACE_HD_REC709 = 2,
-		COLOR_SPACE_SD_REC601 = 3,
-		COLOR_SPACE_MAX = COLOR_SPACE_SD_REC601,
-	};
 	BOOL IsYuvPlanar(VideoPixelFormat format);
 	BOOL IsOpaque(VideoPixelFormat format);
 
